54952ede21834f66ba954ae5670f911e3ad7079e
Oksana <kskaplun@gmail.com>
2022-05-19 15:57:46 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { useState } from 'react';
+
 export const App = () => {
+  const [state, setState] = useState({ isAdmin: false, userName: 'Oksana' });
+  const { isAdmin, userName } = state;
+  const assignAdmin = () => {
+    setState({ ...state, isAdmin: true });
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1
+        style={{
+          margin: '100px auto 0',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#ff0101',
+        }}
+      >
+        Hello {userName}, you are a {isAdmin ? 'SUPER ADMIN' : 'user'}
+      </h1>
+      <button
+        onClick={assignAdmin}
+        style={{
+          margin: '50px auto 0',
+          // height: '100vh',
+          display: 'block',
+          // justifyContent: 'center',
+          // alignItems: 'center',
+          // fontSize: 40,
+          // color: '#010101',
+        }}
+      >
+        Assign admin
+      </button>
+    </>
   );
 };
